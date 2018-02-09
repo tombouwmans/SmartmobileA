b79e522b544dbda4ba33a04bc1f4042900070a52
Fhict <tom.bouwmans@live.nl>
2018-02-09 12:40:57 +0100

    cv app

---
+++ Carnaval/Carnaval/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carnaval
+//
+//  Created by Fhict on 09/02/2018.
+//  Copyright © 2018 Fhict. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
